8dc3a0d7a92ea4fb6dd4cd1f343ff6d28fb87cb0
luke-coldridge <lcoldridge34@gmail.com>
2026-04-08 22:25:20 +0100

    First successful pull of activity details for a single activity. Need to get all activities 200 at a time, and retrieve and cache all activity details 1 at a time

--- get_data.R
+++ get_data.R
@@ -1,5 +1,7 @@
 library(httr) #read the libray httr
 library(jsonlite)
+library(dplyr)
+library(hms)
 
 client_id<-Sys.getenv("CLIENT_ID")
 client_secret<-Sys.getenv("CLIENT_SECRET")
@@ -7,8 +9,9 @@ refresh_token <- readLines(".strava_refresh_token")
 
 
 athlete_url<-"https://www.strava.com/api/v3/athlete"
-activities_url<-"https://www.strava.com/api/v3/athlete/activities"
+activity_list_url<-"https://www.strava.com/api/v3/athlete/activities"
 auth_url<-"https://www.strava.com/oauth/token" #The OAuth endpoint
+details_url<-"https://www.strava.com/api/v3/activities/"
 
 # Get an access_token (reuse the refresh_token we got earlier)
 # FLOW: Use Client Secret & Client ID from .Renv 
@@ -31,8 +34,8 @@ writeLines(refresh_token, ".strava_refresh_token")
 
 print(access_token)
 
-athlete_res<-GET(url=athlete_url, config=add_headers("Authorization"=paste("Basic Authorization: Bearer", access_token, sep= ' ')))
-print(athlete_res)
+athlete_res<-GET(url=athlete_url, 
+                 config=add_headers("Authorization"=paste("Basic Authorization: Bearer", access_token, sep= ' ')))
 
 athlete<-fromJSON(rawToChar(athlete_res$content))
 print(names(athlete))
@@ -41,19 +44,19 @@ print(c(athlete$firstname, athlete$lastname, athlete$updated_at))
 
 
 activities_res<-GET(url=activities_url, add_headers("Authorization"=paste("Basic Authorization: Bearer", access_token, sep= ' ')))
-print(activities_res) #confirm the 200 response
 
 activities<-fromJSON(rawToChar(activities_res$content))
 print(activities)
 
+ids<-activities$id #Get a vector of all activity IDs
 
+#Get activity details for one activity (need to loop over the vector):
+details <- GET(url=paste(details_url, ids[14], sep=''),
+                config=add_headers("Authorization"=paste("Basic Authorization: Bearer", access_token, sep= ' '))) 
 
+single_details<-fromJSON(rawToChar(details$content))
+print(single_details)
 
+best_efforts<-single_details$best_efforts
+best_efforts$moving_time <- hms::as_hms(best_efforts$moving_time) %>% print() #Edit the moving time field to hours:minutes:seconds rather than just seconds
 
-
-# print(res)
-
-
-
-
-# 18c519cb56497e0be0976fc89624862f661971fe
